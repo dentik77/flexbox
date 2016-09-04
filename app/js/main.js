0f814035d0b040d767db06e12c7928a8a50892cb
dentik77 <den_nogin@mail.ru>
2016-09-04 11:17:17 +0300

    top menu s hoverom nugnoperedelivat

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
-
+// sidbar
 	$('.menu-triger').click(function() {
 		$('.navigation-list').slideToggle(500);
-	});
+			});
 	$(window).resize(function() {
 		if ($(window).width()>761){
 			$('.navigation-list').removeAttr('style');
+			
 		}
 	});
+// top-menu
+	$('.topmenu').click(function() {
+		
+
+		$(".top-list").addClass("top-list1"),
+		$('.top-list1').removeClass('top-list');
+
+		// $('top-list').toggleClass('top-list1'),
+		$('.top-list1').slideToggle(500);
+			});
+	$(window).resize(function() {
+		if ($(window).width()>761){
+			// $('.top-list1').css("display", "none"),
+			// $('.top-list1').removeClass('top-list'),
+			$(".top-list1").addClass("top-list"),
+		$('.top-list').removeClass('top-list1');
+			$('.top-list').removeAttr('style');
+			 
+			
+		}
+	});
+
 });
