9ff53d15f34b348f27501c491bfd22b0f52124e1
dentik77 <den_nogin@mail.ru>
2016-09-09 16:15:37 +0300

    dodelan footer est problema v top-menu

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,7 @@ $(document).ready(function() {
 	});
 
 });
+// адаптация верхнего иеню под мобильную версию
 ;(function( $, window, document, undefined )
 {
 	$.fn.doubleTapToGo = function( params )
@@ -73,3 +74,81 @@ $(document).ready(function() {
 // 	$('#nav')
 // 	e.preventDefault();
 // });
+// Модуль добавления формы
+var addForm = (function (){
+
+	var init = function(){
+				// console.log('Инициализация модуля addProject');
+				_setUpListners();
+			};
+		var	_setUpListners = function (){
+				$('#in-form').on('click', _showModalV); // открыть модальное окно
+				$('#registr-form').on('click', _showModalR); // открыть модальное окно
+				$('#toregister').on('click', _showModalR);
+				$('#toin').on('click', _showModalV);//
+				// $('#avtorizaciya').on('submit', _proverForm); //проверка формы
+
+			};
+		var	_showModalV = function (e){
+      	console.log('Вызов модального окна vxod');
+
+   			e.preventDefault();// отмена стандартного поведения (чтобы по нажатию на ссылку не подпрыгивала вверх)
+	    	$('#modal-in').bPopup({
+	    		speed: 650,
+	    		transition: 'slideUp',
+	    		transitionClose: 'slideDown'
+	    		
+	    		
+	    	});
+
+	    };   
+	    var _showModalR = function (e){
+	    console.log('Вызов модального окна registr');
+
+	    	e.preventDefault();
+	    	$('#modal-reg').bPopup({
+	    		speed: 650,
+	    		transition: 'slideUp',
+	    		transitionClose: 'slideDown'
+	    		
+	    	});	
+	    };
+
+	    // var _proverForm = function (e) {
+	    // console.log('Вызов модального окна проверки формы');
+	    // 	e.preventDefault();
+
+	    // 	// объявляем переменные
+	    // 	var form = $(this),
+	 	  // 		url = '_add_form.php',
+	    // 		data = form.serialize();
+
+	    // 		console.log (data);
+	    // 	// запрос на сервер
+	    // 	$.ajax({
+	    // 		url: url,
+	    // 		type: 'POST', // тип запроса
+	    // 		dataType: 'json', // тип данных
+	    // 		data: data,
+
+	    // 	})
+	    // 	.done(function(ans){ // приходит ответ от сервера
+	    // 		console.log("succes");
+	    // 		console.log(ans);
+	    // 	})
+	    // 	.fail(function(){
+	    // 		console.log("error");
+		   // 	})
+	    // 	.always(function(){
+	    // 		console.log("complete");
+	    // 	})
+
+	    // };
+
+	return {
+		init: init
+	};
+
+})();
+
+addForm.init();
